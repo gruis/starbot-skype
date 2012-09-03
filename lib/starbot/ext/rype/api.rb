a54d1c046fd8458b239d86d19c9cebd24ebbc8b0
Gavin Brock <gavin@brock-family.org>
2012-09-03 16:02:57 +0900

    Fixed logger calls

--- lib/starbot/ext/rype/api.rb
+++ lib/starbot/ext/rype/api.rb
@@ -6,11 +6,11 @@ module Rype
     private
 
     def log_incoming(message)
-      STDERR.puts "<- #{message}" if verbose == Logger::DEBUG
+      STDERR.puts "<- #{message}" if verbose == ::Logger::DEBUG
     end
 
     def log_outgoing(message)
-      STDERR.puts "-> #{message}" if verbose == Logger::DEBUG
+      STDERR.puts "-> #{message}" if verbose == ::Logger::DEBUG
     end
   end # class::Api
 end # module::Skype
